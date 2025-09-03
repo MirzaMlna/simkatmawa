a7fffce5b1d97971b9dad0a4af00f4aafc75b19b
MirzaMlna <mirzamaulana713@gmail.com>
2025-09-03 21:02:00 +0800

    ✨

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,12 @@
     <div class="container mx-auto text-center">
         <p class="text-sm text-gray-600">&copy; {{ date('Y') }} Kemahasiswaan UNISKA. Semua Hak Cipta Dilindungi.</p>
         <p class="text-sm text-gray-500">
-            Made with ❤️ by
-            <a href="https://randa.my.id" target="_blank" class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-200">Randa</a>
+            Coded by
+            <a href="https://randa.my.id" target="_blank"
+                class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-200">Randa</a>
             and
-            <a href="https://mirzamlna.github.io/portfolio" target="_blank" class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-200">Mirza</a>
+            <a href="https://mirzamlna.github.io/portfolio" target="_blank"
+                class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-200">Mirza</a>
         </p>
     </div>
 </footer>
