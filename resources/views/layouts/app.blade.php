6df3e8e85f1dace940c4aac8eb4b3c82e7827e7c
vugn <gustiranda3014@gmail.com>
2025-09-04 09:22:43 +0700

    refactor(layouts): improve sidebar and app layout formatting
    - Format mobile sidebar overlay and button elements for better readability
    - Remove footer from app layout and update sidebar footer with credits
    - Adjust spacing and transitions in sidebar content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,29 +22,18 @@
 <body class="h-full font-inter antialiased bg-slate-50">
     <div x-data="{ sidebarOpen: false }" class="h-full">
         <!-- Mobile Sidebar Overlay -->
-        <div 
-            x-show="sidebarOpen" 
-            x-transition:enter="transition-opacity ease-linear duration-300"
-            x-transition:enter-start="opacity-0"
-            x-transition:enter-end="opacity-100"
-            x-transition:leave="transition-opacity ease-linear duration-300"
-            x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0"
-            class="fixed inset-0 z-50 bg-slate-900/80 lg:hidden" 
-            @click="sidebarOpen = false"
-        ></div>
+        <div x-show="sidebarOpen" x-transition:enter="transition-opacity ease-linear duration-300"
+            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+            x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0" class="fixed inset-0 z-50 bg-slate-900/80 lg:hidden"
+            @click="sidebarOpen = false"></div>
 
         <!-- Mobile Sidebar -->
-        <aside 
-            x-show="sidebarOpen"
-            x-transition:enter="transition ease-in-out duration-300 transform"
-            x-transition:enter-start="-translate-x-full"
-            x-transition:enter-end="translate-x-0"
-            x-transition:leave="transition ease-in-out duration-300 transform"
-            x-transition:leave-start="translate-x-0"
+        <aside x-show="sidebarOpen" x-transition:enter="transition ease-in-out duration-300 transform"
+            x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
+            x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0"
             x-transition:leave-end="-translate-x-full"
-            class="fixed inset-y-0 left-0 z-50 w-72 bg-white border-r border-gray-200 shadow-2xl lg:hidden"
-        >
+            class="fixed inset-y-0 left-0 z-50 w-72 bg-white border-r border-gray-200 shadow-2xl lg:hidden">
             @include('layouts.sidebar-content')
         </aside>
 
@@ -59,16 +48,13 @@
             <div class="flex-1 flex flex-col min-w-0">
                 <!-- Top Navigation -->
                 @include('layouts.navigation')
-                
+
                 <!-- Page Content -->
                 <main class="flex-1 overflow-y-auto bg-slate-50">
                     <div class="p-6 lg:p-8">
                         {{ $slot }}
                     </div>
                 </main>
-                
-                <!-- Footer -->
-                @include('layouts.footer')
             </div>
         </div>
     </div>
