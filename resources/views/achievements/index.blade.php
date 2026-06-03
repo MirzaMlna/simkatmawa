e19a38fd6c94d899b423ca678a68b3431d8a8c69
vugn <gustiranda3014@gmail.com>
2026-06-03 09:51:47 +0700

    fix: remove extraneous closing div tag in achievements index view

--- resources/views/achievements/index.blade.php
+++ resources/views/achievements/index.blade.php
@@ -284,7 +284,6 @@
                             </tbody>
                         </table>
                     </div>
-            </div>
 
             <!-- Pagination -->
             <div class="border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
